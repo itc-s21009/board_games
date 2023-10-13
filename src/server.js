2ba1abb8d90ffca362c8e444d45f60d19918b048
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-10-13 19:11:47 +0900

    名前を設定していない場合はゲームが表示されないようにした

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const session = require('express-session')
 const path = require("path");
 const app = express()
 const {Server} = require('socket.io')
 
 const PORT = process.env.port ?? 8080
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+require('dotenv').config()
+
+app.use(session({
+    secret: process.env.SECRET
+}))
 
 app.get('/', (req, res) => {
+    if (!req.session.name) {
+        res.send('名前入力が必要')
+        return
+    }
+    console.log(`name:${req.session.name}`)
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 const server = app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
