0b00ccc3d0a5b4d79a9665766063ba3af7335d15
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-10-14 00:06:15 +0900

    名前設定時に、DBにも保存するようにした

--- src/server/api/name.js
+++ src/server/api/name.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const {name} = req.body
     if (!name) {
         return res.status(500).json({message: 'nameが必要です。'})
     }
     req.session.name = name
-    return res.status(200).json({message: `名前を設定しました：${name}`})
+    await prisma.user.create({
+        data: {
+            sessionId: req.session.id,
+            name: name
+        }
+    }).then(() => {
+        return res.status(200).json({message: `名前を設定しました：${name}`})
+    })
 })
 
 module.exports = router
