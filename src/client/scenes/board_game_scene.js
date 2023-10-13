6ccd56a40d147586a21454fc29d22a90ca82283d
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-10-14 08:04:06 +0900

    ゲーム側から名前を取得できるようにした

--- src/client/scenes/board_game_scene.js
+++ src/client/scenes/board_game_scene.js
@@ -1,23 +1,40 @@
+import {socket} from "../game";
+
 export class BoardGameScene extends Phaser.Scene {
     constructor(key) {
         super({key: key});
     }
 
     init(data) {
-        this.prevScenes = data.prevScenes ?? []
+        this.internalData = data.internalData ?? {}
+
+        this.internalData.prevScenes = this.internalData.prevScenes ?? []
+        if (!this.internalData.playerName) {
+            fetch(`http://localhost:8080/api/name`, {withCredentials: true})
+                .then((r) => r.json())
+                .then((data) => {
+                    console.log('name fetched')
+                    this.internalData.playerName = data.name
+                })
+        }
+
         this.passedData = data ?? {}
-        delete this.passedData.prevScenes
+        delete this.passedData.internalData
+    }
+
+    getPlayerName() {
+        return this.internalData.playerName
     }
 
     moveTo(key, data = {}) {
-        this.prevScenes.push({key: this.scene.key, data: this.passedData})
-        this.scene.start(key, {prevScenes: this.prevScenes, ...data})
+        this.internalData.prevScenes.push({key: this.scene.key, data: this.passedData})
+        this.scene.start(key, {internalData: this.internalData, ...data})
     }
 
     backToPrevScene() {
-        const prevScene = this.prevScenes.pop()
+        const prevScene = this.internalData.prevScenes.pop()
         if (prevScene) {
-            this.scene.start(prevScene.key, {prevScenes: this.prevScenes, ...prevScene.data})
+            this.scene.start(prevScene.key, {internalData: this.internalData, ...prevScene.data})
         }
     }
 }
--- src/server/api/name.js
+++ src/server/api/name.js
@@ -3,6 +3,15 @@ const router = express.Router()
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
+router.get('/', async (req, res) => {
+    const name = req.session.name
+    if (name) {
+        return res.status(200).json({name: name})
+    } else {
+        return res.status(400).json({message: '名前が登録されていません'})
+    }
+})
+
 router.post('/', async (req, res) => {
     const {name} = req.body
     if (!name) {
