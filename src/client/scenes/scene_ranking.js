7403105bd47f2f3c90f6f85b79a5d1e44e9de98d
Asahi Kiyuna <s21009@std.it-college.ac.jp>
2023-11-05 10:42:12 +0900

    データ表示、ゲーム切り替えの処理作成

--- src/client/scenes/scene_ranking.js
+++ src/client/scenes/scene_ranking.js
@@ -1,7 +1,7 @@
 import {BoardGameScene} from "./board_game_scene";
 import {SCENE_RANKING, WIDTH} from "./scene_loader";
 import {createButton, createCircleNumber, createText, drawBackground} from "../components";
-import {COLOR_DIVIDER, COLOR_SECOND, COLOR_THIRD} from "../game";
+import {COLOR_DIVIDER, COLOR_SECOND, COLOR_THIRD, GAME_DAIFUGO, GAME_REVERSI, GAME_SINKEI, GAME_SPEED} from "../game";
 
 export class SceneRanking extends BoardGameScene {
     constructor() {
@@ -20,8 +20,8 @@ export class SceneRanking extends BoardGameScene {
     create() {
         drawBackground(this)
         createText(this, WIDTH / 2, 46, 'ランキング', {fontSize: 48})
-        createButton(this, 29 + 150/2, 104, 150, 50, COLOR_SECOND, '勝利数', {fontSize: 24})
-        createButton(this, 193 + 150/2, 104, 150, 50, COLOR_SECOND, 'レート', {fontSize: 24})
+        const objBtnWins = createButton(this, 29 + 150/2, 104, 150, 50, COLOR_SECOND, '勝利数', {fontSize: 24})
+        const objBtnRating = createButton(this, 193 + 150/2, 104, 150, 50, COLOR_SECOND, 'レート', {fontSize: 24})
 
         this.add.rectangle(19, 217, 8, 344, COLOR_SECOND)
             .setStrokeStyle(1, COLOR_DIVIDER)
@@ -54,38 +54,102 @@ export class SceneRanking extends BoardGameScene {
                 isSvg: true,
                 withoutShadow: true
             })
-        const objTextGame = createText(this, WIDTH / 2, 174, 'オセロ')
+        const objTextGame = createText(this, WIDTH / 2, 174, '')
         const objBtnBack = createButton(this, WIDTH / 2, 582, 330, 69, COLOR_SECOND, '戻る', {fontSize: 24})
         objBtnBack.setOnClick(() => {
             this.backToPrevScene()
         })
 
         const topColors = [0xE4FF41, 0xAFAFAF, 0xCC6200]
-        for (let i = 0; i < 6; i++) {
-            const placement = i + 1
-            const offsetY = i * 55
-            if (placement <= 3) {
-                const color = topColors[i]
-                const objCirclePlacement = createCircleNumber(this, 60, 252 + offsetY, 20, color, placement)
-                objCirclePlacement.setFontSize(32)
-                const objGraphicsPlayer = this.add.graphics()
-                objGraphicsPlayer.fillGradientStyle(color, color, color, color)
-                objGraphicsPlayer.fillRect(90, 232 + offsetY, 205, 40)
-            } else {
-                const objCirclePlacement = createCircleNumber(this, 60, 252 + offsetY, 20, COLOR_SECOND, placement)
-                objCirclePlacement.setFontSize(32)
-                const objGraphicsPlayer = this.add.graphics()
-                objGraphicsPlayer.fillGradientStyle(COLOR_SECOND, COLOR_SECOND, COLOR_SECOND, COLOR_SECOND)
-                objGraphicsPlayer.fillRect(90, 232 + offsetY, 205, 40)
-            }
-            const objTextPlayer = createText(this, 95, 242 + offsetY, `player${placement}`, {fontSize: 16})
-            objTextPlayer.setOrigin(0)
-            const objGraphicsScore = this.add.graphics()
-            objGraphicsScore.fillStyle(COLOR_THIRD)
-            objGraphicsScore.fillRoundedRect(220, 235 + offsetY, 70, 34, 10)
-            const objTextScore = createText(this, 255, 242 + offsetY, '5000', {color: 0xE4FF41, fontSize: 20})
-            const objDivider = this.add.line(26, 279 + offsetY, 0, 0, 279, 0, COLOR_DIVIDER)
-            objDivider.setOrigin(0)
+        const reqData = {
+            type: 'wins',
+            gameName: 'sinkei'
         }
+        const leaderboardObjects = []
+        const displayData = (leaderboard) => {
+            leaderboardObjects.splice(0).forEach((obj) => obj.destroy())
+            const topColors = [0xE4FF41, 0xAFAFAF, 0xCC6200]
+            leaderboard.forEach((data, i) => {
+                const placement = i + 1
+                const offsetY = i * 55
+                if (placement <= 3) {
+                    const color = topColors[i]
+                    const objCirclePlacement = createCircleNumber(this, 60, 252 + offsetY, 20, color, placement)
+                    objCirclePlacement.setFontSize(32)
+                    const objGraphicsPlayer = this.add.graphics()
+                    objGraphicsPlayer.fillGradientStyle(color, color, color, color)
+                    objGraphicsPlayer.fillRect(90, 232 + offsetY, 205, 40)
+                    leaderboardObjects.push(objCirclePlacement, objGraphicsPlayer)
+                } else {
+                    const objCirclePlacement = createCircleNumber(this, 60, 252 + offsetY, 20, COLOR_SECOND, placement)
+                    objCirclePlacement.setFontSize(32)
+                    const objGraphicsPlayer = this.add.graphics()
+                    objGraphicsPlayer.fillGradientStyle(COLOR_SECOND, COLOR_SECOND, COLOR_SECOND, COLOR_SECOND)
+                    objGraphicsPlayer.fillRect(90, 232 + offsetY, 205, 40)
+                    leaderboardObjects.push(objCirclePlacement, objGraphicsPlayer)
+                }
+                const objTextPlayer = createText(this, 95, 242 + offsetY, data.name, {fontSize: 16})
+                objTextPlayer.setOrigin(0)
+                const objGraphicsScore = this.add.graphics()
+                objGraphicsScore.fillStyle(COLOR_THIRD)
+                objGraphicsScore.fillRoundedRect(220, 235 + offsetY, 70, 34, 10)
+                const objTextScore = createText(this, 255, 242 + offsetY, data.score, {color: 0xE4FF41, fontSize: 20})
+                const objDivider = this.add.line(26, 279 + offsetY, 0, 0, 279, 0, COLOR_DIVIDER)
+                objDivider.setOrigin(0)
+                leaderboardObjects.push(objTextPlayer, objGraphicsScore, objTextScore, objDivider)
+            })
+        }
+        const cacheLeaderboard = {}
+        const reqDataToKey = (reqData) => `${reqData.type}_${reqData.gameName}`
+        const fetchData = (reqData) => {
+            this.socketEmit('get_leaderboard', reqData, (leaderboard) => {
+                const reqDataKey = reqDataToKey(reqData)
+                cacheLeaderboard[reqDataKey] = leaderboard
+                displayData(leaderboard)
+            })
+        }
+        const refreshData = (reqData) => {
+            const reqDataKey = reqDataToKey(reqData)
+            const cache = cacheLeaderboard[reqDataKey]
+            if (cache) {
+                displayData(cache)
+            } else {
+                fetchData(reqData)
+            }
+        }
+        let gamePointer = 0
+        const games = [
+            GAME_SPEED,
+            GAME_SINKEI,
+            GAME_DAIFUGO,
+            GAME_REVERSI
+        ]
+        const setGame = (pointer) => {
+            if (pointer < 0) {
+                pointer = games.length - 1
+            }
+            gamePointer = pointer % games.length
+            const game = games[gamePointer]
+            reqData.gameName = game.id
+            objTextGame.text = game.title
+            refreshData(reqData)
+        }
+        const setType = (type) => {
+            reqData.type = type
+            refreshData(reqData)
+        }
+
+        objBtnWins.setOnClick(() => {
+            setType('wins')
+        })
+        objBtnRating.setOnClick(() => {
+            setType('rating')
+        })
+        objBtnNext.setOnClick(() => {
+            setGame(++gamePointer)
+        })
+        objBtnPrev.setOnClick(() => {
+            setGame(--gamePointer)
+        })
     }
 }
